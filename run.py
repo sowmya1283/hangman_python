de62bbd8a470a58a6199c5c49676429bb4ba7b21
sowmya1283 <75764956+sowmya1283@users.noreply.github.com>
2025-02-24 18:38:09 +0000

    Modified the code to add final comments and uodated accroding to pep8

--- run.py
+++ run.py
@@ -1,12 +1,9 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
-
 import random
 from words import WORDS
 
 """
-Dictionary of Key and tuple values, key to describe no of incorrect guesses, tuple to store ascii art character to hangman
+Dictionary of Key and tuple values, key to describe no of incorrect guesses,
+tuple to store ascii art character to hangman
 """
 hangmanart = {
     0: (" ",
@@ -33,7 +30,6 @@ hangmanart = {
 }
 
 
-
 def display_hangman(trials):
     """
     Function to display the hangman
@@ -44,7 +40,7 @@ def display_hangman(trials):
     print("********")
 
 
-def display_hint(hint):    
+def display_hint(hint):
     """
     Function to display the hint
     """
@@ -53,7 +49,7 @@ def display_hint(hint):
 
 def display_answer(answer):
     """
-    Function to display the answer 
+    Function to display the answer
     """
     print(" ".join(answer))
 
@@ -78,52 +74,54 @@ def menu():
             can_continue = True
             display_instructions()
         elif choice == "3":
-            can_continue == True
+            can_continue = True
             print("Thank you for playing Hangman")
             exit()
         else:
             print("Invalid choice. Please try again")
 
 
-def play_game(): 
-    answer = random.choice(WORDS)  #randomly select a word from the list
-    hint = ["_"] * len(answer)  #create a hint list with the same length as the answer
+def play_game():
+    """
+    Function to play the game
+    Randomly select a word from the list and start the game
+    Then check the guessed letter with the answer
+    and display the hangman and hint
+    """
+    answer = random.choice(WORDS)
+    hint = ["_"] * len(answer)
+    trials = 0
 
-    trials = 0    #initialize the number of trials (Wrong answers) to 0
-   
-    guessed_letters = set()    #initialize the set of guessed letters in a set
-    is_running = True #When initially run this program, set the value to true, once we finish the game turn this to a false
-
+    guessed_letters = set()
+    is_running = True
 
     while is_running:
-        display_hangman(trials) #call the function to display the hangman
-        display_hint(hint) #call the function to display the hint
-        #display_answer(answer)  #call the function to display the answer (for testing purposes)
-        guess = input("enter a letter: ").lower() #ask the user to enter a letter and convert it to lowercase
+        display_hangman(trials)
+        display_hint(hint)
+        guess = input("enter a letter: ").lower()
 
-        if len(guess) != 1 or not guess.isalpha(): #check if the user entered more than one letter
+        if len(guess) != 1 or not guess.isalpha():
             print("Invalid input")
             continue
 
-        if guess in guessed_letters: #check if the guess is in the set of guessed letters
+        if guess in guessed_letters:
             print(f"You already guessed the letter {guess}")
             continue
 
-        guessed_letters.add(guess) #add the guess to the set of guessed letters
-        
-        if guess in answer: #check if the guess is in the answer
+        guessed_letters.add(guess)
+        if guess in answer:
             for i in range(len(answer)):
                 if answer[i] == guess:
-                    hint[i] =guess
+                    hint[i] = guess
         else:
             trials += 1
 
-        if "_" not in hint: #check if the hint list has no more underscores
+        if "_" not in hint:
             display_hangman(trials)
             display_answer(answer)
             print("YOU WIN!")
             is_running = False
-        elif trials >= len(hangmanart) -1: #check if the number of trials is greater than or equal to the length of the hangman art
+        elif trials >= len(hangmanart) - 1:
             display_hangman(trials)
             display_answer(answer)
             print("YOU LOSE!")
@@ -131,6 +129,9 @@ def play_game():
 
 
 def display_instructions():
+    """
+    Function to display the instructions of the game
+    """
     print("Instructions to play the game")
     print("1. The computer will randomly select a word from the list")
     print("2. You have to guess the word by entering a letter")
@@ -138,12 +139,15 @@ def display_instructions():
     print("4. If you guess the word before 6 chances, you win")
     print("5. If you don't guess the word before 6 chances, you lose")
     input("Please enter to go back to the menu")
-    menu()    
+    menu()
 
 
 def main():
-    menu() #main function
-   
-    
+    """
+    Main function calling the menu to start the game
+    """
+    menu()
+
+
 if __name__ == "__main__":
     main()
